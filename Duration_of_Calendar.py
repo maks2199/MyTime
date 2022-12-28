028a06d2fa760c558e8421af8b3ec8974b1cb298
Gitpshe <ae.eau.e@gmail.com>
2022-12-29 00:38:53 +0300

    Add Duration_of_Calendar

--- Duration_of_Calendar.py
+++ Duration_of_Calendar.py
@@ -1,120 +1,105 @@
-import datetime  # Импорт библиотеки для работы с датой и временем
-import os # Импорт библиотеки для работы с файловой системой
-import matplotlib.pyplot as plt
+import datetime  # Библиотека для работы с датой и временем
+import os # Библиотека для работы с файловой системой
+import matplotlib.pyplot as plt # Библиотека для визуализации
 
-# Основная папка
+############################################################################################
+################    Открытие файлов календаря           ####################################
+############################################################################################
+
+# Основная папка для хранения календарей
 directory = 'C:/Users\Ae Eau\Desktop\Все папки\Программирование и автоматизация\Анализ данных\Гугл календарь\Подсчет времени календаря\Calendars'
 
-# Список имен календарей
+# Список имен файлов календарей
 list_calendars = os.listdir(directory)
-print(list_calendars)
-
+# Список имен календарей
 list_name_of_calendars = []
-
 # Список значений длительностей
 list_calendars_duration = []
 
 # Для каждого календаря из папки
 for calendar in list_calendars:
-    # Открываем входные данные файлов календарей
-    path_calendar = directory + '/' + calendar
-    name_of_calendar = calendar[:-4]
-    list_name_of_calendars.append(name_of_calendar)
-
-    print(list_calendars)
-    #print (path_calendar)
-
+    path_calendar = directory + '/' + calendar  # Путь к данному календарю
+    name_of_calendar = calendar[:-4]  # Имя календаря без расширения файла
+    list_name_of_calendars.append(name_of_calendar)  # Список имен календарей
+    # Открываем календари по их пути в режиме чтения
     file_calendar = open(path_calendar, 'r', encoding='utf-8')
-    file_calendar_content = file_calendar.read()
-
-    #print(file_calendar_content)
-
-    list_files = [] #
-
-    str(file_calendar_content)
-
-    CommonStartData = str(file_calendar_content)
+    file_calendar_content = file_calendar.read()  # Заносим содержимое календаря в память
+    str_file_calendar_content = str(file_calendar_content)  # Приводим содержимое календаря к строке
 
     ############################################################################################
     ################    Фильтрация строк с датой          ######################################
     ############################################################################################
 
-    SeparatedStr = CommonStartData.split('\n') # Разделяю исходную строку на строки по переносу строки
-    #print(SeparatedStr)  # Проверяю, что строки разделились верно
-
+    list_simple_str = str_file_calendar_content.split('\n') # Разделяю исходную строку по символу переноса строки
 
     fragment1 = 'DTSTART:2022'  # Задаю фрагмент, по которому буду фильтровать строки
     fragment2 = 'DTEND:2022'  # Задаю фрагмент по которому буду фильтровать строки
-    listDate = []  # Задаю пустой список для заполнения строками с датой
-    for SimpleStr in SeparatedStr: # Для каждой строки из списка
-        if fragment1 in SimpleStr or fragment2 in SimpleStr: # Если она содержит дату начала или конца
-            listDate.append(SimpleStr)  # Добавляю ее в список дат
-    #print(listDate)  # Проверяю, что даты отфильтрвались верно
+    list_date = []  # Задаю пустой список для заполнения строками с датой
+    for simple_str in list_simple_str: # Для каждой строки из списка строк
+        if fragment1 in simple_str or fragment2 in simple_str: # Если она содержит дату начала или конца
+            list_date.append(simple_str)  # Добавляю ее в список дат
 
     ############################################################################################
     ################    Преобразование формата даты и времени          #########################
     ################    Нахождение разницы дат                         #########################
-    ################    Суммирование разниц                            #########################
+    ################    Суммирование времени                            ########################
     ############################################################################################
 
     index1 = 0  # Задаю стартовый индекс элемента из списка дат
     sumtime = datetime.timedelta (0, 0, 0, 0, 0)  # Задаю стартовое значение для суммарного времени на календарь
 
 
-    for date_ in listDate:  # Для каждой даты из списка дат
+    for date_ in list_date:  # Для каждой даты из списка дат
         if index1 % 2 == 0: # Если индекс четный
-            strDateStart = listDate[index1]  # То это дата старта события
-            #print(strDateStart)  # Проверяем, что вывелась дата старта
+            str_date_start = list_date[index1]  # То это дата старта события
 
-            # Вычисляем полное время старта события по индексам симвлов в строке и приводим к целому
-            yearStart = int(strDateStart[8] + strDateStart[9] + strDateStart[10] + strDateStart[11])  # Год
-            monthStart = int(strDateStart[12] + strDateStart[13]) # Месяц
-            dayStart = int(strDateStart[14] + strDateStart[15])  # День
-            hourStart = int(strDateStart[17] + strDateStart[18])  # Час
-            minStart = int(strDateStart[19] + strDateStart[20])  # Минута
+            # Вычисляем полное время старта события по индексам символов в строке и приводим к целому типу данных
+            year_start = int(str_date_start[8] + str_date_start[9] + str_date_start[10] + str_date_start[11])  # Год
+            month_start = int(str_date_start[12] + str_date_start[13]) # Месяц
+            day_start = int(str_date_start[14] + str_date_start[15])  # День
+            hour_start = int(str_date_start[17] + str_date_start[18])  # Час
+            minute_start = int(str_date_start[19] + str_date_start[20])  # Минута
 
             #print(yearStart, monthStart, dayStart, hourStart, minStart)  # Проверяю, что дата старта вывелась верно
-            dt1 = datetime.datetime(yearStart, monthStart, dayStart, hourStart, minStart)  # Приводим дату старта к формату даты
+            date1 = datetime.datetime(year_start, month_start, day_start, hour_start, minute_start)  # Приводим дату старта к формату даты
             index1 += 1  # Переходим к следующему индексу
         else:
-            strDateEnd = listDate[index1] # Остальные строки - даты конца
-            #print(strDateEnd)  # Проверяем, что вывелась дата конца
+            str_date_end = list_date[index1] # Остальные строки - даты конца
 
             # Вычисляем полное время конца события по индексам (отличаются от предыдущего) символов в строке и приводим к целому
-            yearEnd = int(strDateEnd[6] + strDateEnd[7] + strDateEnd[8] + strDateEnd[9]) # Год
-            monthEnd = int(strDateEnd[10] + strDateEnd[11])  # Месяц
-            dayEnd = int(strDateEnd[12] + strDateEnd[13])  # День
-            hourEnd = int(strDateEnd[15] + strDateEnd[16])  # Час
-            minEnd = int(strDateEnd[17] + strDateEnd[18])  # Минута
+            year_end = int(str_date_end[6] + str_date_end[7] + str_date_end[8] + str_date_end[9]) # Год
+            month_end = int(str_date_end[10] + str_date_end[11])  # Месяц
+            day_end = int(str_date_end[12] + str_date_end[13])  # День
+            hour_end = int(str_date_end[15] + str_date_end[16])  # Час
+            minute_end = int(str_date_end[17] + str_date_end[18])  # Минута
 
             #print(yearEnd, monthEnd, dayEnd, hourEnd, minEnd)   # Проверяю, что дата конца вывелась верно
-            dt2 = datetime.datetime(yearEnd, monthEnd, dayEnd, hourEnd, minEnd)  # Приводим дату конца к формату даты
+            date2 = datetime.datetime(year_end, month_end, day_end, hour_end, minute_end)  # Приводим дату конца к формату даты
 
-            tdelta = dt2 - dt1  # Считаю продолжительность данного события
+            date_delta = date2 - date1  # Считаю продолжительность данного события
             #print(tdelta)  # Проверяю, что вывелась продолжительность события
-            # print(tdelta.total_seconds())
-            # print(type(tdelta.total_seconds()))
-            sumtime += tdelta # увеличиваю суммарное время на значение продолжительности соыбтия
+            sumtime += date_delta # увеличиваю суммарное время на значение продолжительности события
             index1 += 1  # Переходим к следующему индексу
-    list_calendars_duration.append(sumtime)
-    print("sumtime: ",sumtime)
-    print(list_calendars_duration)
-    print('Итого потрачено времени: ', calendar, sumtime)  # ВЫВОЖУ ИТОГОВОЕ СУММАРНОЕ ВРЕМЯ
 
-    ############################################################################################
-    ################    Визуализация данный                            #########################
-    ############################################################################################
-list_calendars_duration_seconds = []
+    list_calendars_duration.append(sumtime)  # Добавляем длительность календаря в список длительностей календарей
+    print('Итого потрачено времени за 2022 год на', name_of_calendar, ':', sumtime)  # ВЫВОЖУ ИТОГОВОЕ СУММАРНОЕ ВРЕМЯ
 
-for duration in list_calendars_duration:
-    duration_seconds = duration.total_seconds()
-    list_calendars_duration_seconds.append(duration_seconds)
-print(list_calendars_duration_seconds)
+############################################################################################
+################    Визуализация данных                            #########################
+############################################################################################
 
-labels = list_name_of_calendars
-print(len(labels))
-sizes = list_calendars_duration_seconds
-print(len(sizes))
+# Перевод длительностей в секунды
+
+list_calendars_duration_seconds = []  # Задаем список длительностей в секундах
+
+for duration in list_calendars_duration:  # Для каждой длительности из календаря
+    duration_seconds = duration.total_seconds()  # Переводим длительность в секундах
+    list_calendars_duration_seconds.append(duration_seconds)  # Добавляем длительности (сек) в список длительностей (сек)
+
+# Визуализация
+
+labels = list_name_of_calendars  # Названия участков диаграммы соответствуют названиям календарей
+sizes = list_calendars_duration_seconds  # Размеры участков диаграммы соответствуют длительности календаря (сек)
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
