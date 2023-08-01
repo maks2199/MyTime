9039c0eaf85c56b0b1793fd27f5bbcb94e70c52a
maks2199 <maks21992199@mail.ru>
2023-08-01 22:36:47 +0300

    authorization added, but bad

--- MyTime.py
+++ MyTime.py
@@ -1,7 +1,19 @@
+import asyncio
+import os
+
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+
+from httpx_oauth.clients.google import GoogleOAuth2
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 import api
 import analyzer
@@ -10,15 +22,77 @@ from pages import page_result, page_pie_chart
 
 st.set_page_config(layout="wide")
 # --------------------------------------------------------------------------------------------------------------------
-if st.button("Sign in with Google"):
-    # Redirect the user to the Google Sign-In page
-    auth_url = "https://accounts.google.com/o/oauth2/auth"
-    client_id = "564171152911-3f6baosrv1eg82qk8itf9rldk8o0i605.apps.googleusercontent.com"  # Replace with your actual client ID
-    redirect_uri = "http://localhost:8501"  # Replace with your redirect URI
-    scope = "https://www.googleapis.com/auth/calendar.readonly"  # Replace with the desired scopes
-    state = "12345"  # Replace with a unique state value
-    auth_endpoint = f"{auth_url}?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}"
-    st.markdown(f'<a href="{auth_endpoint}">Click here to sign in with Google</a>', unsafe_allow_html=True)
+# AUTHORIZATION
+
+# if st.button("Sign in with Google"):
+#     # Redirect the user to the Google Sign-In page
+#     auth_url = "https://accounts.google.com/o/oauth2/auth"
+#     client_id = "564171152911-3f6baosrv1eg82qk8itf9rldk8o0i605.apps.googleusercontent.com"  # Replace with your actual client ID
+#     redirect_uri = "http://localhost:8501"  # Replace with your redirect URI
+#     scope = "https://www.googleapis.com/auth/calendar.readonly"  # Replace with the desired scopes
+#     state = "12345"  # Replace with a unique state value
+#     auth_endpoint = f"{auth_url}?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}"
+#     st.markdown(f'<a href="{auth_endpoint}">Click here to sign in with Google</a>', unsafe_allow_html=True)
+
+# client_id = os.environ['GOOGLE_CLIENT_ID']
+# client_secret = os.environ['GOOGLE_CLIENT_SECRET']
+# redirect_uri = os.environ['REDIRECT_URI']
+client_id = '564171152911-3f6baosrv1eg82qk8itf9rldk8o0i605.apps.googleusercontent.com'
+client_secret = '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'
+redirect_uri = 'http://localhost:8501'
+
+client = GoogleOAuth2(client_id, client_secret)
+
+
+async def write_authorization_url(client,
+                                  redirect_uri):
+    authorization_url = await client.get_authorization_url(
+        redirect_uri,
+        scope=["https://www.googleapis.com/auth/calendar.readonly"],
+        extras_params={"access_type": "offline"},
+    )
+    return authorization_url
+
+
+authorization_url = asyncio.run(
+    write_authorization_url(client=client,
+                            redirect_uri=redirect_uri)
+)
+
+st.write(f'''<h1>
+    Please login using this <a target="_self"
+    href="{authorization_url}">url</a></h1>''',
+         unsafe_allow_html=True)
+
+code = st.experimental_get_query_params()['code']
+
+
+async def write_access_token(client,
+                             redirect_uri,
+                             code):
+    token = await client.get_access_token(code, redirect_uri)
+    return token
+
+
+if 'token' not in st.session_state:
+    token = asyncio.run(
+        write_access_token(client=client,
+                           redirect_uri=redirect_uri,
+                           code=code))
+    token['scope'] = 'https://www.googleapis.com/auth/calendar.readonly'
+    st.session_state['token'] = token
+    print('token:')
+    print(token)
+
+
+# SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+if 'token' in st.session_state:
+    creds = Credentials(token=st.session_state['token']['access_token'])
+    service = build('calendar', 'v3', credentials=creds)
+
+app = api.App(service)
+# ----------------------------------------------------------------------------------------------------------------
 
 # WEB-PAGE LAYOUT
 st.title('⌚ MyTime ')
@@ -53,7 +127,7 @@ def side_bar_time():
 
             # Getting timetable from Google Calendar API
             with st.spinner("Getting timetable from Google Calendar"):
-                df_main = api.get_time_table(time_min, time_max)
+                df_main = app.get_time_table(time_min, time_max)
                 st.session_state['df_main'] = df_main
         else:
             st.write('Choose options and press the button to create chart')
--- api.py
+++ api.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import os.path
 from pprint import pprint
 
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -18,260 +17,217 @@ import pandas as pd
 import streamlit as st
 from http import cookies
 
-
 import matplotlib.pyplot as plt
 
 
-#######################################################################################################################
-# Functions
-#######################################################################################################################
+class App():
 
+    def __init__(self, service):
+        self.service = service
 
-# def get_google_id_token(auth_code, client_id):
-#     try:
-#         token_ = id_token.fetch_id_token(requests.Request(), auth_code, client_id)
-#         return token_
-#     except ValueError as e:
-#         st.error(f"Error retrieving ID token: {e}")
-#
-# import streamlit.ReportThread as ReportThread
-# from streamlit.web.server import Server
-# # Handle OAuth2 callback route
-# AUTHORIZATION_CODE = "code"
-# REDIRECT_URI = "http://localhost:8501/"  # Replace with your redirect URI
-#
-# if st._is_running_with_streamlit:
-#     ctx = ReportThread.get_report_ctx()
-#     session_id = ctx.session_id
-#     session_info = Server.get_current()._session_info_by_id[session_id]
-#
-#     if session_info.ws.request_info.request_line == "GET /":
-#         auth_code = session_info.ws.request_info.query_params[AUTHORIZATION_CODE]
-#         get_google_id_token(auth_code, "564171152911-3f6baosrv1eg82qk8itf9rldk8o0i605.apps.googleusercontent.com")  # Replace with your actual client ID
-#         # Continue with the rest of your app's logic
+    def get_ten_upcoming_events(self, calendar_id):
+        # Call the Calendar API
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = self.service.events().list(calendarId=calendar_id, timeMin=now,
+                                                   maxResults=10, singleEvents=True,
+                                                   orderBy='startTime').execute()
+        events = events_result.get('items', [])
+        return events
 
-def get_ten_upcoming_events(calendar_id):
-    # Call the Calendar API
-    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId=calendar_id, timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-    return events
+    def get_all_calendar_events(self, calendar_id):
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        events_result = self.service.events().list(calendarId=calendar_id, singleEvents=True, timeMax=now).execute()
+        events = events_result.get('items', [])
+        return events
 
+    def get_calendar_events(self, calendar_id, time_min, time_max):
+        """Returns events from time_min to time_max for calendar by its id"""
+        events_result = self.service.events().list(calendarId=calendar_id, singleEvents=True,
+                                              timeMin=time_min, timeMax=time_max).execute()
+        events = events_result.get('items', [])
+        return events
 
-def get_all_calendar_events(calendar_id):
-    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    events_result = service.events().list(calendarId=calendar_id, singleEvents=True, timeMax=now).execute()
-    events = events_result.get('items', [])
-    return events
+    def calculate_event_duration(self, event):
+        return self.get_event_end_time(event) - self.get_event_start_time(event)
 
+    def get_event_start_time(self, event):
+        _string = event.get('start', dict()).get('dateTime')
+        if _string is not None:
+            # return datetime.fromisoformat(_string)
+            return datetime.strptime(_string, "%Y-%m-%dT%H:%M:%S%z")
+        else:
+            return 0
 
-def get_calendar_events(calendar_id, time_min, time_max):
-    """Returns events from time_min to time_max for calendar by its id"""
-    events_result = service.events().list(calendarId=calendar_id, singleEvents=True,
-                                          timeMin=time_min, timeMax=time_max).execute()
-    events = events_result.get('items', [])
-    return events
+    def get_event_end_time(self, event):
+        _string = event.get('end', dict()).get('dateTime')
+        if _string is not None:
+            # return datetime.fromisoformat(_string)
+            return datetime.strptime(_string, "%Y-%m-%dT%H:%M:%S%z")
+        else:
+            return 0
 
+    def duration_to_seconds(self, duration):
+        """Converts duration from dateTime format to seconds"""
+        if isinstance(duration, int):
+            duration = pd.to_timedelta(duration)
+        return duration.total_seconds()
 
-def calculate_event_duration(event):
-    return get_event_end_time(event) - get_event_start_time(event)
+    #######################################################################################################################
+    # Authorization
+    #######################################################################################################################
 
+    # creds = None
+    # if os.path.exists('token.json'):
+    #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             'credentials.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open('token.json', 'w') as token:
+    #         token.write(creds.to_json())
 
-def get_event_start_time(event):
-    _string = event.get('start', dict()).get('dateTime')
-    if _string is not None:
-        # return datetime.fromisoformat(_string)
-        return datetime.strptime(_string, "%Y-%m-%dT%H:%M:%S%z")
-    else:
-        return 0
+    #######################################################################################################################
+    # Application
+    #######################################################################################################################
+    def get_time_table(self, time_min, time_max):
+        print()
+        print("------------------")
+        print("GETTING TIME TABLE")
+        print("------------------")
+        # Printing progress bar
+        # i = 0
 
+        # Converting time to needed format
+        # Adding time to date from streamlit
+        time_min = datetime.combine(time_min, datetime.min.time())
+        time_max = datetime.combine(time_max, datetime.max.time())
 
-def get_event_end_time(event):
-    _string = event.get('end', dict()).get('dateTime')
-    if _string is not None:
-        # return datetime.fromisoformat(_string)
-        return datetime.strptime(_string, "%Y-%m-%dT%H:%M:%S%z")
-    else:
-        return 0
+        # Calculating total seconds in this range
+        total_delta = time_max - time_min
+        total_date_range_seconds = total_delta.total_seconds()
+        # print('TOTAL total_date_range_seconds')
+        # print(total_date_range_seconds)
 
+        # Converting to isoformat (string)
+        time_min = datetime.isoformat(time_min) + '+03:00'
+        time_max = datetime.isoformat(time_max) + '+03:00'
 
-def duration_to_seconds(duration):
-    """Converts duration from dateTime format to seconds"""
-    if isinstance(duration, int):
-        duration = pd.to_timedelta(duration)
-    return duration.total_seconds()
-
-
-#######################################################################################################################
-# Authorization
-#######################################################################################################################
-
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-
-creds = None
-if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
-
-service = build('calendar', 'v3', credentials=creds)
-
-
-#######################################################################################################################
-# Application
-#######################################################################################################################
-def get_time_table(time_min, time_max):
-    print()
-    print("------------------")
-    print("GETTING TIME TABLE")
-    print("------------------")
-    # Printing progress bar
-    # i = 0
-
-    # Converting time to needed format
-    # Adding time to date from streamlit
-    time_min = datetime.combine(time_min, datetime.min.time())
-    time_max = datetime.combine(time_max, datetime.max.time())
-
-    # Calculating total seconds in this range
-    total_delta = time_max - time_min
-    total_date_range_seconds = total_delta.total_seconds()
-    # print('TOTAL total_date_range_seconds')
-    # print(total_date_range_seconds)
-
-    # Converting to isoformat (string)
-    time_min = datetime.isoformat(time_min) + '+03:00'
-    time_max = datetime.isoformat(time_max) + '+03:00'
-
-    # print('CALENDAR LIST:')
-    calendar_list_result = service.calendarList().list().execute()
-    # pprint(calendar_list_result)
-    calendar_list = calendar_list_result.get('items', [])
-    # pprint(calendar_list)
-    # print()
-    # print("Calendar[0]: ")
-    # pprint(calendar_list[0])
-
-    # pandas table
-    table = []
-
-    for calendar in calendar_list:
-
-        calendar_name = calendar.get('summary')
-        calendar_color = calendar.get('backgroundColor')
-
-        id_ = calendar.get('id')
-        # print(calendar_name, id_)
-
-        # print('EVENTS:')
-        events_ = get_calendar_events(id_, time_min, time_max)
-
+        # print('CALENDAR LIST:')
+        calendar_list_result = self.service.calendarList().list().execute()
+        # pprint(calendar_list_result)
+        calendar_list = calendar_list_result.get('items', [])
+        # pprint(calendar_list)
         # print()
-        # print("Events[0]: ")
-        # print(type(events_))
-        # if len(events_) > 0:
-        #     pprint(events_[0])
+        # print("Calendar[0]: ")
+        # pprint(calendar_list[0])
 
-        for event_ in events_:
-            # Printing progress bar
-            # i += 1
-            # if i % 100 == 0:
-            #     print('processed event ', i)
+        # pandas table
+        table = []
 
-            row = dict()
-            row['Calendar'] = calendar_name
-            row['Calendar color'] = calendar_color
-            # pprint(event_)
-            # print(event_['summary'])
-            # if event_['summary'] is None:
-            #     event_name = '-'
-            # else:
-            #     event_name = event_['summary']
-            row['Event'] = event_.get('summary')
-            # print(calculate_event_duration(event_))
-            row['Duration'] = calculate_event_duration(event_)
-            # pprint(event_)
-            # print(type(row['Duration']))
-            row['Duration seconds'] = duration_to_seconds(row['Duration'])
-            row['Start time'] = get_event_start_time(event_)
-            row['End time'] = get_event_end_time(event_)
+        for calendar in calendar_list:
 
-            table.append(row)
+            calendar_name = calendar.get('summary')
+            calendar_color = calendar.get('backgroundColor')
 
-    df = pd.DataFrame(table)
+            id_ = calendar.get('id')
+            # print(calendar_name, id_)
 
-    total_event_seconds = df['Duration seconds'].sum()
-    # print()
-    # print('total_event_seconds: ', total_event_seconds)
-    unfilled_event_seconds = total_date_range_seconds - total_event_seconds
-    # TOD correct unfilled_event_seconds, events can intersect!
+            # print('EVENTS:')
+            events_ = self.get_calendar_events(id_, time_min, time_max)
 
-    row = dict()
-    row['Calendar'] = 'Unfilled'
-    row['Event'] = 'Unfilled event'
-    row['Duration'] = '-'
-    row['Duration seconds'] = unfilled_event_seconds
+            # print()
+            # print("Events[0]: ")
+            # print(type(events_))
+            # if len(events_) > 0:
+            #     pprint(events_[0])
 
-    df = df.append(row, ignore_index=True)
+            for event_ in events_:
+                # Printing progress bar
+                # i += 1
+                # if i % 100 == 0:
+                #     print('processed event ', i)
 
-    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print()
-    print("Raw data frame: ")
-    print(df)
+                row = dict()
+                row['Calendar'] = calendar_name
+                row['Calendar color'] = calendar_color
+                # pprint(event_)
+                # print(event_['summary'])
+                # if event_['summary'] is None:
+                #     event_name = '-'
+                # else:
+                #     event_name = event_['summary']
+                row['Event'] = event_.get('summary')
+                # print(calculate_event_duration(event_))
+                row['Duration'] = self.calculate_event_duration(event_)
+                # pprint(event_)
+                # print(type(row['Duration']))
+                row['Duration seconds'] = self.duration_to_seconds(row['Duration'])
+                row['Start time'] = self.get_event_start_time(event_)
+                row['End time'] = self.get_event_end_time(event_)
 
-    df.to_csv('raw_time.csv')
+                table.append(row)
 
-    return df
+        df = pd.DataFrame(table)
 
+        total_event_seconds = df['Duration seconds'].sum()
+        # print()
+        # print('total_event_seconds: ', total_event_seconds)
+        unfilled_event_seconds = total_date_range_seconds - total_event_seconds
+        # TOD correct unfilled_event_seconds, events can intersect!
 
-def get_calendars_list():
-    calendar_list_result = service.calendarList().list().execute()
-    calendar_list = calendar_list_result.get('items', [])
-    return calendar_list
+        row = dict()
+        row['Calendar'] = 'Unfilled'
+        row['Event'] = 'Unfilled event'
+        row['Duration'] = '-'
+        row['Duration seconds'] = unfilled_event_seconds
 
+        df = df.append(row, ignore_index=True)
 
-def get_groped_events(df):
-    df_grouped_events = df.loc[:, ['Event', 'Duration seconds']]
-    df_grouped_events = df_grouped_events.groupby('Event').sum()
-    df_grouped_events = df_grouped_events.reset_index()
+        # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print()
+        print("Raw data frame: ")
+        print(df)
 
-    # print('Events')
-    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    #     print(df_grouped_events)
+        df.to_csv('raw_time.csv')
 
-    return df_grouped_events
+        return df
 
+    def get_calendars_list(self):
+        calendar_list_result = self.service.calendarList().list().execute()
+        calendar_list = calendar_list_result.get('items', [])
+        return calendar_list
 
-def get_calendar_events_table(df, calendar_name):
-    df_calendar_events = df.loc[df['Calendar'] == calendar_name]
-    df_calendar_events = df_calendar_events.loc[:, ['Event', 'Duration']]
-    return df_calendar_events
+    def get_groped_events(self, df):
+        df_grouped_events = df.loc[:, ['Event', 'Duration seconds']]
+        df_grouped_events = df_grouped_events.groupby('Event').sum()
+        df_grouped_events = df_grouped_events.reset_index()
 
+        # print('Events')
+        # with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        #     print(df_grouped_events)
 
+        return df_grouped_events
 
+    def get_calendar_events_table(self, df, calendar_name):
+        df_calendar_events = df.loc[df['Calendar'] == calendar_name]
+        df_calendar_events = df_calendar_events.loc[:, ['Event', 'Duration']]
+        return df_calendar_events
 
+    def create_days_plot(self, table_by_days):
+        table_by_days = table_by_days.loc['Статья', :]
+        table_by_days = table_by_days.reset_index()
+        print(table_by_days)
 
-def create_days_plot(table_by_days):
-    table_by_days = table_by_days.loc['Статья', :]
-    table_by_days = table_by_days.reset_index()
-    print(table_by_days)
+        x = table_by_days.loc[:, ['Start date']]
+        y = table_by_days.loc[:, ['Duration seconds']]
 
-    x = table_by_days.loc[:, ['Start date']]
-    y = table_by_days.loc[:, ['Duration seconds']]
+        # plot_ = plt.plot(x, y)
+        # plt.show()
 
-    # plot_ = plt.plot(x, y)
-    # plt.show()
-
-    return table_by_days
+        return table_by_days
