cbff5527acb56fea2be30e426857121cf5a1ffe3
maks2199 <maks21992199@mail.ru>
2024-02-08 22:05:01 +0300

    docker configs

--- MyTime.py
+++ MyTime.py
@@ -47,17 +47,17 @@ async def write_access_token(client,
 # --------------------------------------------------------------------------------------------------------------------
 # AUTHORIZATION
 
-# client_id = os.environ['GOOGLE_CLIENT_ID']
-# client_secret = os.environ['GOOGLE_CLIENT_SECRET']
-# redirect_uri = os.environ['REDIRECT_URI']
-client_id = credentials.client_id
-client_secret = credentials.client_secret
+client_id = os.environ['GOOGLE_CLIENT_ID']
+client_secret = os.environ['GOOGLE_CLIENT_SECRET']
+redirect_uri = os.environ['REDIRECT_URI']
+# client_id = credentials.client_id
+# client_secret = credentials.client_secret
 
 # Local
 # redirect_uri = 'http://localhost:8501'
 # TODOo:: change when deploy
 # Docker
-redirect_uri = configs.redirect_uri
+# redirect_uri = configs.redirect_uri
 
 client = GoogleOAuth2(client_id, client_secret)
 
